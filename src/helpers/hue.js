cd9cf7770b113e590daefecb5528efa547c085f9
JClerc <jices25@gmail.com>
2016-12-11 01:11:19 +0100

    chore(hue): add comments

--- src/helpers/hue.js
+++ src/helpers/hue.js
@@ -13,31 +13,45 @@ const lightState = hue.lightState
 
 export const run = async (options: Options): Promise<void> => {
   try {
+    // Begin by searching for bridges
     if (options.debug) console.log('Search Hue bridge ... 🔥')
     const bridges = await hue.nupnpSearch()
+
+    // Search ended
     if (bridges.length > 0) {
       if (options.debug) console.log('Bridges found: ', bridges)
 
       // Use the first one
       const bridge = bridges[0]
+
+      // Make API and light state
       const api = new HueApi(bridge.ipaddress, options.username)
       const state = lightState.create()
+
+      // Make an array with lights
       const bridgeLights = (await api.lights()).lights.map(light => parseInt(light.id))
       const lights = options.lights
         ? options.lights.filter(id => bridgeLights.includes(id))
         : bridgeLights
 
+      // Random int from range 0 - 255
       const rand = () => Math.floor((Math.random() * 255))
 
+      // And start the loop
       const loop = setInterval(async () => {
+        // lights array is empty
         if (lights.length === 0) {
           clearInterval(loop)
           console.error('lights is empty, stopping loop...')
         }
 
+        // Define light state
         state.on().rgb(rand(), rand(), rand()).brightness(1)
+
+        // Iterate over each light
         lights.forEach(async (id) => {
           try {
+            // And apply new light state
             const result = await api.setLightState(id, state)
             if (result !== true && options.debug) {
               console.error('Cannot change light state', result)
