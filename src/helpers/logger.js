724ca383141209f4e970aff8216f401da1733d3a
JClerc <jices25@gmail.com>
2016-12-11 20:35:28 +0100

    feat(logger): improve logging system

---
+++ src/helpers/logger.js
@@ -0,0 +1,61 @@
+
+import colors from 'colors/safe'
+
+let globalLogLevel = 0
+
+class Logger {
+
+  constructor(prefix, logLevel) {
+    this.prefix = `[${prefix}]`
+    this.logLevel = typeof logLevel === 'undefined'
+      ? Logger.getGlobalLevel
+      : logLevel
+  }
+
+  error(...items) {
+    if (this.getLevel() >= 0) {
+      this.colorlog(colors.red, items)
+    }
+  }
+
+  warn(...items) {
+    if (this.getLevel() >= 1) {
+      this.colorlog(colors.yellow, items)
+    }
+  }
+
+  info(...items) {
+    if (this.getLevel() >= 2) {
+      this.colorlog(colors.blue, items)
+    }
+  }
+
+  log(level, ...items) {
+    if (typeof level !== 'number') {
+      throw new Error('level must be a number')
+    }
+    if (this.getLevel() >= level) {
+      this.colorlog(colors.blue, items)
+    }
+  }
+
+  getLevel() {
+    if (typeof this.logLevel === 'function') {
+      return this.logLevel()
+    }
+    return this.logLevel
+  }
+
+  colorlog(color, items) {
+    console.log.apply(this, [color(this.prefix)].concat(items))
+  }
+}
+
+// Static methods
+Logger.setGlobalLevel = level => globalLogLevel = level
+Logger.getGlobalLevel = () => globalLogLevel
+
+// Global logger
+Logger.app = new Logger('app')
+
+export default Logger
